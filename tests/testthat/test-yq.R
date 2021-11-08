8b561bc36f0fe9c6a9041b3525b8e92d1f6a0d2d
Jiri Novotny <jiri.novotny@img.cas.cz>
2021-11-08 14:38:34 +0100

    modify test-yq.R according to the previous commit; do not test yq merge by comparing file contents, but by means of R objects

--- tests/testthat/test-yq.R
+++ tests/testthat/test-yq.R
@@ -8,10 +8,12 @@ test_that("yq tool's binary is successfully downloaded or reused", {
   skip_on_cran()
 
   tmpfile <- fs::file_temp()
-  expect_type(download_yq(destfile = tmpfile, set_as_default = FALSE, ask = FALSE, overwrite = TRUE), "character")
-  expect_true(fs::file_exists(tmpfile))
-  expect_invisible(check_yq(yq_binary = tmpfile))
-  expect_null(download_yq(destfile = tmpfile, set_as_default = FALSE, ask = FALSE))
+  res <- download_yq(destfile = tmpfile, set_as_default = FALSE, ask = FALSE, overwrite = TRUE)
+  expect_type(res, "character")
+  yq_binary <- res[1]
+  expect_true(fs::file_exists(yq_binary))
+  expect_invisible(check_yq(yq_binary = yq_binary))
+  expect_null(download_yq(destfile = yq_binary, set_as_default = FALSE, ask = FALSE))
 })
 
 test_that("yq merge shell command works", {
@@ -22,7 +24,8 @@ test_that("yq merge shell command works", {
 
   tmpfile <- fs::file_temp()
   yq_merge_cmd("yq_test_files/src.yaml", "yq_test_files/dest.yaml", stdout = tmpfile)
-  expect_snapshot_file(tmpfile, name = "yq_merge_cmd.yaml", cran = TRUE)
+  yaml <- yaml::yaml.load_file(tmpfile)
+  expect_identical(yaml, list(a = "aa", b = "bb", c = "c"))
 
   expect_error(yq_merge_cmd("non_existing_file", "yq_test_files/dest.yaml"))
   expect_error(yq_merge_cmd("yq_test_files/src.yaml", "non_existing_file"))
