4a69b602c8721c58945eb53e385a95eca4873c2d
Jiri Novotny <jiri.novotny@img.cas.cz>
2022-08-18 13:38:37 +0200

    cleanup

--- dev/run_pipeline_test.R
+++ dev/run_pipeline_test.R
@@ -1,27 +1,16 @@
-withr::with_envvar(
-  new = list(
-    SCDRAKE_TEST_DOWNLOAD_YQ = "FALSE",
-    SCDRAKE_TEST_RUN_PIPELINE = "TRUE",
-    SCDRAKE_TEST_RUN_PIPELINE_SINGLE_SAMPLE_FULL = "TRUE",
-    SCDRAKE_TEST_RUN_PIPELINE_INTEGRATION = "TRUE",
-    SCDRAKE_TEST_RUN_PIPELINE_BASE_OUT_DIR = fs::path_abs("../scdrake_run_pipeline_test_output_8")
-  ),
-  code = {
-    devtools::test(filter = "run_pipeline")
-  }
-)
+run_pipeline_test <- function(output_dir) {
+  withr::with_envvar(
+    new = list(
+      SCDRAKE_TEST_DOWNLOAD_YQ = "FALSE",
+      SCDRAKE_TEST_RUN_PIPELINE = "TRUE",
+      SCDRAKE_TEST_RUN_PIPELINE_SINGLE_SAMPLE_FULL = "TRUE",
+      SCDRAKE_TEST_RUN_PIPELINE_INTEGRATION = "TRUE",
+      SCDRAKE_TEST_RUN_PIPELINE_BASE_OUT_DIR = fs::path_abs(output_dir)
+    ),
+    code = {
+      devtools::test(filter = "run_pipeline")
+    }
+  )
+}
 
-# callr::r(
-#   devtools::test,
-#   args = list(filter = "run_pipeline"),
-#   env = c(
-#     callr::rcmd_safe_env(),
-#     SCDRAKE_TEST_DOWNLOAD_YQ = "FALSE",
-#     SCDRAKE_TEST_RUN_PIPELINE = "TRUE",
-#     SCDRAKE_TEST_RUN_PIPELINE_SINGLE_SAMPLE_FULL = "TRUE",
-#     SCDRAKE_TEST_RUN_PIPELINE_INTEGRATION = "TRUE",
-#     SCDRAKE_TEST_RUN_PIPELINE_BASE_OUT_DIR = "/mnt/foibe3/Users/novotnyj/projects/bioinfocz/scdrake_run_pipeline_test_output_3"
-#   ),
-#   show = TRUE,
-#   package = "devtools"
-# )
+run_pipeline_test("../scdrake_run_pipeline_test_output_10")
